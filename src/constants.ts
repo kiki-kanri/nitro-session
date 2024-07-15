14226032beafa6f5d702b65c62587f35f195d694
kiki-kanri <a470666@gmail.com>
2024-07-16 03:22:03 +0800

    refactor: change `defaultStorageOptions` to `defaultOptions`

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,13 @@ import type { PluginOptions } from './types/options';
 
 export const changedSymbol = Symbol();
 export const clearedSymbol = Symbol();
-export const defaultStorageOptions: Readonly<Required<NonNullable<PluginOptions['storage']>>> = { data: { driver: 'memory' }, token: { driver: 'cookie' } };
+export const defaultOptions: Readonly<Required<PluginOptions>> = {
+	enabled: true,
+	maxAge: 86400,
+	storage: {
+		data: { driver: 'memory' },
+		token: { driver: 'cookie' }
+	}
+};
+
 export const unstorageKeySymbol = Symbol();
