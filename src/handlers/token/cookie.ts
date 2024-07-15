531c819669261b11102569aa1cd10aabf5c03476
kiki-kanri <a470666@gmail.com>
2024-07-16 02:35:47 +0800

    feat: add `CookieTokenHandler` and `HeaderTokenHandler`

---
+++ src/handlers/token/cookie.ts
@@ -0,0 +1,41 @@
+import type { CookieSerializeOptions } from 'cookie-es';
+import { deleteCookie, getCookie, setCookie } from 'h3';
+import type { H3Event } from 'h3';
+import { merge, omit } from 'lodash-es';
+
+import type { TokenStorageOptions } from '../../types/options';
+
+export class CookieTokenHandler {
+	#name: string;
+	#serializeOptions: CookieSerializeOptions;
+
+	constructor(options: TokenStorageOptions.Cookie['options'], maxAge: number) {
+		this.#name = options?.name || 'session';
+		this.#serializeOptions = {
+			...merge(
+				{
+					httpOnly: true,
+					path: '/',
+					sameSite: 'lax',
+					secure: true
+				},
+				omit(options, 'name')
+			),
+			maxAge
+		};
+	}
+
+	delete(event: H3Event) {
+		deleteCookie(event, this.#name, this.#serializeOptions);
+	}
+
+	get(event: H3Event) {
+		return getCookie(event, this.#name);
+	}
+
+	set(event: H3Event, value: string) {
+		setCookie(event, this.#name, value, this.#serializeOptions);
+	}
+}
+
+export default CookieTokenHandler;
---
+++ src/handlers/token/header.ts
@@ -0,0 +1,28 @@
+import { getHeader, setHeader } from 'h3';
+import type { H3Event } from 'h3';
+
+import type { TokenStorageOptions } from '../../types/options';
+
+export class HeaderTokenHandler {
+	#name: string;
+	#setName: string;
+
+	constructor(options: TokenStorageOptions.Header['options']) {
+		this.#name = options?.name || 'session';
+		this.#setName = options?.setName || 'set-session';
+	}
+
+	delete(event: H3Event) {
+		setHeader(event, this.#setName, '');
+	}
+
+	get(event: H3Event) {
+		return getHeader(event, this.#name);
+	}
+
+	set(event: H3Event, value: string) {
+		setHeader(event, this.#setName, value);
+	}
+}
+
+export default HeaderTokenHandler;
