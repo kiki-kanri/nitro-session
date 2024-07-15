908adcdcb8276143278a36efad2ba8e87cbcce1f
kiki-kanri <a470666@gmail.com>
2024-07-16 03:25:44 +0800

    feat: initial completion of core functionality

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,58 @@
+import consola from 'consola';
+import { cloneDeep, merge } from 'lodash-es';
+import { useRuntimeConfig } from 'nitropack/runtime';
 import type { NitroApp } from 'nitropack';
 
-export default (nitroApp: NitroApp) => {
-	console.log(nitroApp);
+import { changedSymbol, clearedSymbol, defaultOptions } from './constants';
+import { DataHandler } from './handlers/data';
+import CookieTokenHandler from './handlers/token/cookie';
+import HeaderTokenHandler from './handlers/token/header';
+
+import type { PluginOptions } from './types/options';
+import type { PartialH3EventContextSession } from './types/session';
+import { setupH3EventContextSession } from './utils';
+
+export const registerHooks = async (nitroApp: NitroApp, options: Required<PluginOptions>) => {
+	const dataHandler = await DataHandler.createInstance(options);
+	let tokenHandler;
+	if (options.storage?.token?.driver === 'cookie') tokenHandler = new CookieTokenHandler(options.storage.token.options, options.maxAge);
+	else if (options.storage?.token?.driver === 'header') tokenHandler = new HeaderTokenHandler(options.storage?.token?.options);
+	else throw new Error('Invalid token storage driver');
+	nitroApp.hooks.hook('beforeResponse', async (event) => {
+		if (!event.context.session[changedSymbol] || !event.path.startsWith('/api')) return;
+		if (event.context.session[clearedSymbol]) {
+			const token = tokenHandler.get(event);
+			if (token) await dataHandler.delete(token);
+			tokenHandler.delete(event);
+		} else {
+			const token = await dataHandler.setAndGetToken(event.context.session);
+			if (token) tokenHandler.set(event, token);
+		}
+	});
+
+	nitroApp.hooks.hook('request', async (event) => {
+		if (!event.path.startsWith('/api')) return;
+		const token = tokenHandler.get(event);
+		let sessionData: PartialH3EventContextSession | undefined;
+		if (token) {
+			sessionData = await dataHandler.get(token);
+			if (!sessionData) {
+				await dataHandler.delete(token);
+				tokenHandler.delete(event);
+			}
+		}
+
+		setupH3EventContextSession(event, sessionData || {});
+	});
+};
+
+export default async (nitroApp: NitroApp) => {
+	const runtimeConfig = useRuntimeConfig();
+	const pluginOptions = merge(defaultOptions, cloneDeep(runtimeConfig.nitroSession || {}));
+	if (!pluginOptions.enabled) return consola.info('Nitro session disabled.');
+	consola.info('Initializing Nitro session...');
+	consola.info(`Nitro session configured data with '${pluginOptions.storage?.data?.driver}' driver.`);
+	consola.info(`Nitro session configured token with '${pluginOptions.storage?.token?.driver}' driver.`);
+	await registerHooks(nitroApp, pluginOptions);
+	consola.success('Nitro session initialization successful.');
 };
