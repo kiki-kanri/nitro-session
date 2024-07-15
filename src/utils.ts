099901d4e2cbefbb61859990e935b457ade454e4
kiki-kanri <a470666@gmail.com>
2024-07-16 02:35:58 +0800

    feat: add `setupH3EventContextSession` utility

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+import type { H3Event } from 'h3';
+import onChange from 'on-change';
+
+import { changedSymbol } from './constants';
+import type { PartialH3EventContextSession } from './types/session';
+
+export const setupH3EventContextSession = (event: H3Event, session: PartialH3EventContextSession, onChangeCallback?: (event: H3Event) => void) => {
+	event.context.session = onChange(
+		session,
+		() => {
+			event.context.session[changedSymbol] = true;
+			onChangeCallback?.(event);
+			onChange.unsubscribe(event.context.session);
+		},
+		{ ignoreSymbols: true }
+	);
+};
