853a81a775e929cecda687fa07ba3d92d3e99f74
kiki-kanri <a470666@gmail.com>
2024-07-19 01:24:38 +0800

    chore: update dependencies and modify export method of utils in playground

--- playground/utils/session.ts
+++ playground/utils/session.ts
@@ -1,1 +1,1 @@
-export { clearH3EventContextSession, deleteH3EventContextSessionStorageData, getH3EventContextSessionToken, popH3EventContextSession } from '../../src/runtime/utils';
+export * from '../../src/runtime/utils';
