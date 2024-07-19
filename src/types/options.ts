eb21471abf02ff2fd727ca557111b592dc4184af
kiki-kanri <a470666@gmail.com>
2024-07-19 17:05:06 +0800

    docs: add comments to some properties in options

--- src/types/options.ts
+++ src/types/options.ts
@@ -13,17 +13,51 @@ export type DataStorageOptions =
 export type TokenStorageOptions = TokenStorageOptions.Cookie | TokenStorageOptions.Header;
 
 export interface PluginOptions {
+	/**
+	 * @default true
+	 */
 	enabled?: boolean;
+
+	/**
+	 * Session expiration time in seconds.
+	 *
+	 * If the token driver is set to cookie, this value will be used as the `maxAge` option for the cookie.
+	 *
+	 * @default 86400
+	 */
 	maxAge?: number;
+
+	/**
+	 * Options for storing session data and token.
+	 */
 	storage?: {
+		/**
+		 * @default { driver: 'memory' }
+		 */
 		data?: DataStorageOptions;
+
+		/**
+		 * @default { driver: 'cookie' }
+		 */
 		token?: TokenStorageOptions;
 	};
 }
 
 interface UseUnstorageDataStorageOptions {
+	/**
+	 * Options related to keys for storing data in unstorage drivers.
+	 */
 	key?: {
+		/**
+		 * For security reasons, this value must not be less than 24.
+		 *
+		 * @default 24
+		 */
 		length?: number;
+
+		/**
+		 * @default 'session'
+		 */
 		prefix?: string;
 	};
 }
@@ -31,9 +65,32 @@ interface UseUnstorageDataStorageOptions {
 export namespace DataStorageOptions {
 	export interface CookieOrHeader {
 		driver: 'cookie/header';
+
+		/**
+		 * Options for data encryption and decryption.
+		 */
 		options: {
+			/**
+			 * The encoding used for encryption, decryption, and handling keys.
+			 *
+			 * @default
+			 * {
+			 *   decryptInput: 'base64',
+			 *   decryptOutput: 'utf8',
+			 *   encryptInput: 'utf8',
+			 *   encryptOutput: 'base64',
+			 *   key: 'utf8',
+			 *   iv: 'base64'
+			 * }
+			 */
 			encodingOptions?: AESCipherEncodingOptions;
 			encryptionMode?: 'cbc' | 'cfb' | 'cfb1' | 'cfb8' | 'ctr' | 'ofb';
+
+			/**
+			 * The key used for encryption, with a length of 16, 24, or 32 bytes.
+			 *
+			 * The byte length may vary with the same input depending on the encoding type set in `encodingOptions.key`.
+			 */
 			key: string;
 		};
 	}
@@ -71,14 +128,42 @@ export namespace DataStorageOptions {
 export namespace TokenStorageOptions {
 	export interface Cookie {
 		driver: 'cookie';
-		options?: CookieSerializeOptions & { name?: string };
+		/**
+		 * @default
+		 * {
+		 *   httpOnly: true,
+		 *   name: 'session',
+		 *   path: '/',
+		 *   sameSite: 'lax',
+		 *   secure: true
+		 * }
+		 */
+		options?: CookieSerializeOptions & {
+			/**
+			 * The cookie name for storing the token.
+			 *
+			 * @default 'session'
+			 */
+			name?: string;
+		};
 	}
 
 	export interface Header {
 		driver: 'header';
 		options?: {
-			setName?: string;
+			/**
+			 * The key for retrieving the token header when receiving a request.
+			 *
+			 * @default 'session'
+			 */
 			name?: string;
+
+			/**
+			 * The key for setting the token header when returning a response.
+			 *
+			 * @default 'set-session'
+			 */
+			setName?: string;
 		};
 	}
 }
