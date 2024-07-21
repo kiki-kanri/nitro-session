1e9bc8b29308ef46abf97c51d8ea931039947b01
kiki-kanri <a470666@gmail.com>
2024-07-21 23:35:27 +0800

    build: switch build process to Rollup

---
+++ rollup.config.ts
@@ -0,0 +1,36 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
+import del from 'rollup-plugin-delete';
+import nodeExternals from 'rollup-plugin-node-externals';
+
+export default defineConfig({
+	input: ['./src/index.ts', './src/runtime/utils.ts'],
+	output: {
+		dir: './dist',
+		entryFileNames: '[name].mjs',
+		externalLiveBindings: false,
+		format: 'esm',
+		generatedCode: {
+			arrowFunctions: true,
+			constBindings: true,
+			objectShorthand: true
+		},
+		interop: 'compat',
+		preserveModules: true,
+		preserveModulesRoot: './src'
+	},
+	plugins: [
+		del({ targets: './dist' }),
+		nodeExternals({
+			include: [
+				'cookie-es',
+				'h3',
+				'ohash'
+			]
+		}),
+		nodeResolve(),
+		typescript({ tsconfig: './tsconfig.build.json' })
+
+	]
+});
