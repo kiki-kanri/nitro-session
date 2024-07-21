beb319db1ae139a80eb5f68215d5b9033d465a43
kiki-kanri <a470666@gmail.com>
2024-07-22 02:59:05 +0800

    feat: add CJS format to build output

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,25 +1,29 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
+import type { OutputOptions } from 'rollup';
 import del from 'rollup-plugin-delete';
 import nodeExternals from 'rollup-plugin-node-externals';
 
+const generateOutputOptions = (format: 'cjs' | 'esm'): OutputOptions => ({
+	dir: './dist',
+	entryFileNames: `[name].${format === 'cjs' ? 'cjs' : 'mjs'}`,
+	exports: 'named',
+	externalLiveBindings: false,
+	format,
+	generatedCode: {
+		arrowFunctions: true,
+		constBindings: true,
+		objectShorthand: true
+	},
+	interop: 'compat',
+	preserveModules: true,
+	preserveModulesRoot: './src'
+});
+
 export default defineConfig({
 	input: ['./src/index.ts', './src/runtime/utils.ts'],
-	output: {
-		dir: './dist',
-		entryFileNames: '[name].mjs',
-		externalLiveBindings: false,
-		format: 'esm',
-		generatedCode: {
-			arrowFunctions: true,
-			constBindings: true,
-			objectShorthand: true
-		},
-		interop: 'compat',
-		preserveModules: true,
-		preserveModulesRoot: './src'
-	},
+	output: [generateOutputOptions('cjs'), generateOutputOptions('esm')],
 	plugins: [
 		del({ targets: './dist' }),
 		nodeExternals({
