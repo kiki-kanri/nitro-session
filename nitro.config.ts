15aa4d60f3206cda1bca5425e8d16efe61c43adc
kiki-kanri <a470666@gmail.com>
2024-10-02 23:05:08 +0800

    chore: update nitro.config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,21 +1,17 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
 	alias: { '@': '~/' },
+	compatibilityDate: '2100-01-01',
 	noPublicDir: true,
 	serveStatic: false,
 	srcDir: './playground',
 	sourceMap: false,
+	timing: true,
 	typescript: {
 		strict: true,
 		tsConfig: {
-			compilerOptions: {
-				baseUrl: '../../',
-				noImplicitOverride: true,
-				noUncheckedIndexedAccess: true,
-				noUnusedLocals: true,
-				noUnusedParameters: true,
-				paths: { '@/*': ['./playground/*'] }
-			}
+			compilerOptions: { paths: { '@/*': ['../../playground/*'] } },
+			extends: '@kikiutils/tsconfigs/esnext/esnext.json'
 		}
 	}
 });
